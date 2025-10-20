a756692b3cd2c4d9193a40bbc4d2d1b106c0f0c4
Yahel Perez <geracharlie@outlook.com>
2025-10-19 22:25:03 -0600

    Feat: Complete Sprint 2 with environment profiles and config files

--- order-management/src/main/java/com/meli/ordermanagement/OrderManagementApplication.java
+++ order-management/src/main/java/com/meli/ordermanagement/OrderManagementApplication.java
@@ -3,11 +3,12 @@ package com.meli.ordermanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// We are explicitly telling Spring to scan all sub-packages within "com.meli.ordermanagement"
+@SpringBootApplication(scanBasePackages = "com.meli.ordermanagement")
 public class OrderManagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OrderManagementApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OrderManagementApplication.class, args);
+    }
 
 }
